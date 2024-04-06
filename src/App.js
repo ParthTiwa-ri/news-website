03d00abbcbf940bdc6e3cf36a557c56d3bac98df
Parth Tiwari <parthtiwari@Parths-MacBook-Air.local>
2024-04-06 15:34:48 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "./components/common/header/Header";
-import "./App.css";
+
 import Homepages from "./components/home/Homepages";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
